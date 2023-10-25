b01dd5ab5d2386ed7c721258fd59a252fc08fbc1
Dhaval Mehta <20968146+dhaval-mehta@users.noreply.github.com>
2023-10-26 00:52:53 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='app-name',
+    version='0.1',
+    author='Dhaval Mehta',
+    description='Unofficial chittorgarh.com client',
+    url='https://github.com/dhaval-mehta/chittorgarh-client',
+    keywords='development, setup, setuptools',
+    python_requires='>=3.7, <4',
+    packages=find_packages(include=['chittorgarh-client', 'chittorgarh-client.*']),
+    install_requires=[
+        'requests',
+        'lxml',
+    ],
+)
