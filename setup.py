f2f42ad95502b99ac323190419d4381cb7f6c438
Dhaval Mehta <20968146+dhaval-mehta@users.noreply.github.com>
2023-10-26 01:28:48 +0530

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chittorgarh_client',
-    version='0.1',
+    version='0.1.1',
     author='Dhaval Mehta',
     description='Unofficial chittorgarh.com client',
     long_description='Unofficial chittorgarh client',
